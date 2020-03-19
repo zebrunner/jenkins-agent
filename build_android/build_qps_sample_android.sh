6c0ba6d0f0743724b4051440ad6f856b304be91c
Maxim Golokhov <hexwer@gmail.com>
2020-03-20 00:55:50 +0300

    divide android-alpine image to multiple images: base gradle, base android, specific android api (28)

--- build_android/build_qps_sample_android.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -xe
-git clone https://github.com/qaprosoft/qps-sample-android.git
-cd qps-sample-android
-gradle wrapper
-./gradlew assembleDebug
--- build_android/prepare_docker_image.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-set -xe
-docker build -t android_container:qps --file Dockerfile .
