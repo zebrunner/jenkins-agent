408482168079cfe46f10e7919fdca9f4fc9f2162
azarouski <azarouski@solvd.com>
2023-05-18 18:20:21 +0300

    script improvements were made

--- entrypoint.sh
+++ entrypoint.sh
@@ -21,12 +21,6 @@ if [ "$AVD" ]; then
     socat tcp-listen:5555,bind=127.0.0.1,fork tcp:$AVD & SOCAT_PID=$!
 fi
 
-ERROR="\033[0;31m"
-SUCCESS="\033[0;32m"
-HEADER="\033[0;36m"
-ATTENTION="\033[0;33m"
-RESET="\033[0m"
-
 swarmUrl="$JENKINS_MASTER_URL/swarm/swarm-client.jar"
 swarmPath="/swarm-client.jar"
 
@@ -40,50 +34,50 @@ attemptTimeout() {
 }
 
 while [ $(( startTime + SWARM_RESPONSE_TIMEOUT )) -gt "$(date +%s)" ]; do
-  echo -e "${HEADER}Requesting $swarmPath:${RESET}"
+  echo -e "Requesting $swarmPath:"
   responseStatus=$( curl --silent --head $swarmUrl | grep -i "HTTP.\+\d\{3\}" | grep -o "\d\d\d.\+\S" )
   responseStatusCode=$(grep -o "\d\d\d" <<< "$responseStatus")
   if [ $responseStatusCode -eq 200 ]; then
-    echo -e "${SUCCESS}'$swarmUrl' response is '$responseStatus'${RESET}"
+    echo -e "'$swarmUrl' response is '$responseStatus'"
 
-    echo -e "${HEADER}Downloading $swarmPath:${RESET}"
+    echo -e "Downloading $swarmPath:"
     downloadError=$( curl -L $swarmUrl --output $swarmPath 2> >(tee /dev/stderr) )
     downloadStatus=$?
     if [ "$downloadStatus" -eq 0 ]; then
-      echo -e "${SUCCESS}'$swarmPath' file was successfully downloaded${RESET}"
+      echo -e "'$swarmPath' file was successfully downloaded"
     else
-      echo -e "${ERROR}'$swarmPath' file not downloaded.\nCurl exit code ($downloadStatus)\nCurl error:\n$downloadError${RESET}"
+      echo -e "'$swarmPath' file not downloaded.\nCurl exit code ($downloadStatus)\nCurl error:\n$downloadError"
       attemptTimeout
       continue
     fi
 
-    echo -e "${HEADER}Checking $swarmPath size and existence:${RESET}"
+    echo -e "Checking $swarmPath size and existence:"
     test -s $swarmPath
     fileStatus=$?
     if [ $fileStatus -eq 0 ]; then
-      echo -e "${SUCCESS}'$swarmPath' file was successfully found${RESET}"
+      echo -e "'$swarmPath' file was successfully found"
       success=0
       break
     else
-      echo -e "${ERROR}There is no file '$swarmPath' or it's size equal 0${RESET}"
+      echo -e "There is no file '$swarmPath' or it's size equal 0"
       attemptTimeout
       continue
     fi
   else
-    echo -e "${ERROR}'$swarmUrl' response '$responseStatus'${RESET}"
+    echo -e "'$swarmUrl' response '$responseStatus'"
     attemptTimeout
   fi
 done
 
 if [ "$success" -eq 0 ]; then
-  echo -e "${HEADER}Starting $swarmPath:${RESET}"
+  echo -e "Starting $swarmPath:"
 
   swarmMessage=$( start 2> >(tee /dev/stderr) )
   swarmStatus=$?
   echo $swarmMessage
   if [ "$swarmStatus" -ne 0 ]; then
-    echo -e "${ATTENTION}\nErrors occurred on swarm start:${RESET}" 2> >(tee /dev/stderr)
-    echo -e "${ERROR}Error code ($swarmStatus):\n\t$swarmMessage${RESET}" 2> >(tee /dev/stderr)
+    echo -e "\nErrors occurred on swarm start:" 2> >(tee /dev/stderr)
+    echo -e "Error code ($swarmStatus):\n\t$swarmMessage" 2> >(tee /dev/stderr)
 
     sleep $period
     exit 1
@@ -94,17 +88,17 @@ if [ "$success" -eq 0 ]; then
   fi
 
 else
-  echo -e "${ATTENTION}\nErrors occurred on preparing steps:${RESET}" 2> >(tee /dev/stderr)
+  echo -e "\nErrors occurred on preparing steps:" 2> >(tee /dev/stderr)
   if [ "$responseStatusCode" -ne 200 ]; then
-    echo -e "${ERROR}Swarm url response error:\n\t$responseStatus${RESET}" 2> >(tee /dev/stderr)
+    echo -e "Swarm url response error:\n\t$responseStatus" 2> >(tee /dev/stderr)
   fi
 
   if [ ! -z $downloadError ]; then
-    echo -e "${ERROR}Download error ($downloadStatus):\n\t$downloadError${RESET}" 2> >(tee /dev/stderr)
+    echo -e "Download error ($downloadStatus):\n\t$downloadError" 2> >(tee /dev/stderr)
   fi
 
   if [ ! -z $fileStatus ]; then
-    echo -e "${ERROR}File state error:\n\t$fileStatus${RESET}" 2> >(tee /dev/stderr)
+    echo -e "File state error:\n\t$fileStatus" 2> >(tee /dev/stderr)
   fi
 
   exit 1
